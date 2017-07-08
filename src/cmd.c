c115a83a7912c91977e387bc7e12f05979d15b26
k21971 <keith.simpson1971@gmail.com>
2017-07-08 16:49:34 -0400

    Conduct: Elbereth tracking

--- src/cmd.c
+++ src/cmd.c
@@ -1828,19 +1828,6 @@ int final;
 		enl_msg(You_, "have not wished", "did not wish",
 			" for any artifacts");
 	}
-	
-#ifdef ELBERETH_CONDUCT
-#ifdef ELBERETH
-	/* no point displaying the conduct if Elbereth doesn't do anything */
-	if (u.uconduct.elbereths) {
-		Sprintf(buf, "engraved Elbereth %ld time%s",
-			u.uconduct.elbereths, plur(u.uconduct.elbereths));
-		you_have_X(buf);
-	} else {
-		you_have_never("engraved Elbereth");
-	}
-#endif /* ELBERETH */
-#endif /* ELBERETH_CONDUCT */
 
 	/* Pop up the window and wait for a key */
 	display_nhwindow(en_win, TRUE);
@@ -1935,6 +1922,19 @@ int final;
 	    if (!u.uconduct.wisharti)
 		dump("", "  You did not wish for any artifacts");
 	}
+	
+#ifdef ELBERETH_CONDUCT
+#ifdef ELBERETH
+	/* no point displaying the conduct if Elbereth doesn't do anything */
+	if (u.uconduct.elbereths) {
+		Sprintf(buf, "engraved Elbereth %ld time%s",
+			u.uconduct.elbereths, plur(u.uconduct.elbereths));
+		you_have_X(buf);
+	} else {
+		you_have_never("engraved Elbereth");
+	}
+#endif /* ELBERETH */
+#endif /* ELBERETH_CONDUCT */
 
 	dump("", "");
 }
