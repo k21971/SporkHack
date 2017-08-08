f86359248d2d3865be5307aa87a56cc99713b801
k21971 <keith.simpson1971@gmail.com>
2017-08-08 00:16:50 -0400

    add Dungeon Map Overview 3 patch

--- src/read.c
+++ src/read.c
@@ -708,6 +708,7 @@ forget_map(howmuch)
 		levl[zx][zy].seenv = 0;
 		levl[zx][zy].waslit = 0;
 		levl[zx][zy].glyph = cmap_to_glyph(S_stone);
+		levl[zx][zy].styp = STONE;
 	    }
 }
 
@@ -768,6 +769,7 @@ forget_levels(percent)
 	count = ((count * percent) + 50) / 100;
 	for (i = 0; i < count; i++) {
 	    level_info[indices[i]].flags |= FORGOTTEN;
+	    forget_mapseen(indices[i]);
 	}
 }
 
